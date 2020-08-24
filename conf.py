9e3e7156acdcfbcaa79c1f57a2537d0bd15036c9
tonyfast <tony.fast@gmail.com>
2020-08-24 17:23:02 -0400

    dl hackmd

--- conf.py
+++ conf.py
@@ -1,4 +1,20 @@
-import hackmd
+import pathlib
+import requests
+id = "N1naMCFAQb-J0FmQroVlyw"
+
+
+revisions = F"https://hackmd.io/{id}/revision"
+
+response = requests.get(revisions)
+
+entry = requests.get(
+    F"{response.url}/{response.json()['revision'][0]['time']}")
+
+pathlib.Path('index.md').write_text(
+    entry.json()['content']
+)
+
+
 BLOG_TITLE = title = html_title = "Writing docs sucks!"
 BLOG_AUTHOR = author = "Quansight"
 html_theme = "pydata_sphinx_theme"
